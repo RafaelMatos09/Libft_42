c0d895cea75986f23fe3e6df662b64fcb47b9eee
rafmorei <rafael.mattos09@gmail.com>
2023-10-16 22:27:38 -0300

    functions

---
+++ ft_isascii.c
@@ -0,0 +1,10 @@
+#include "libft.h"
+
+int isascii(int c)
+{
+    if (c >= 0 && c <= 127)
+    {
+        return (1);
+    }
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -7,5 +7,8 @@
 
 int ft_strlen(char *str);
 int ft_isalpha(int c);
-
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int isascii(int c);
+int ft_isprint(int c);
 #endif
