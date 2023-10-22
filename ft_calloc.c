8bb586651d4e6dbe38e8f208fc71d5b55189b3a3
rafmorei <rafael.mattos09@gmail.com>
2023-10-22 05:26:40 -0300

    ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *ft_calloc(size_t nitems, size_t size)
+{
+    char *dest;
+    int i;
+
+    dest = malloc(nitems * size);
+    if (dest == NULL)
+        return (NULL);
+    i = 0;
+    while (i < (nitems * size))
+    {
+        dest[i] = 0;
+        i++;
+    }
+    return (dest);
+}
+
+int main(void)
+{
+    char *ptr;
+
+    ptr = ft_calloc(10, 1);
+    ptr = "teste";
+    printf("%s\n ", ptr);
+}
