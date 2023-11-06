1f0b0ae82bfe80898cd474248902343abf131e67
rafmorei <rafael.mattos09@gmail.com>
2023-11-06 03:33:26 -0300

    bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,6 +1,36 @@
-#include "libft.h"
+#include <stdio.h>
 
 void ft_bzero(void *s, size_t n)
 {
-    ft_memset(s, 0, n);
+    unsigned char *str;
+    int i;
+
+    str = (unsigned char*)s;
+    i = 0;
+    while(i < n)
+    {
+        str[i] = '\0';
+        i++;
+    }
+}
+
+int main() {
+    unsigned char buffer[10] = "teste";  
+    size_t n = sizeof(buffer);  
+
+    printf("Antes de chamar ft_bzero:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%02X ", buffer[i]);  
+    }
+    printf("\n");
+
+    ft_bzero(buffer, n);  // Chame a função ft_bzero
+
+    
+    for (int i = 0; i < n; i++) {
+        printf("%02X ", buffer[i]);  
+    }
+    printf("\n");
+
+    return 0;
 }
